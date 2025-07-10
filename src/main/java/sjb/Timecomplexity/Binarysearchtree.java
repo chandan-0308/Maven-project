9c1067e537a551d7ad6ec5f32a9d237f70eb613b
chandan-0308 <smileychandu142@gmail.com>
2025-07-10 16:17:25 +0530

    binary search tree with preorder,inorder and postorder

--- src/main/java/sjb/Timecomplexity/Binarysearchtree.java
+++ src/main/java/sjb/Timecomplexity/Binarysearchtree.java
@@ -26,6 +26,7 @@ public class Binarysearchtree
 	}
 	void inorder()
 	{
+		System.out.println("inorder");
 		inorderNode(root);
 	}
 	void inorderNode(TreeNode node)
@@ -37,13 +38,48 @@ public class Binarysearchtree
 			inorderNode(node.right);
 		}
 	}
+	void preorder()
+	{
+		System.out.println("preorder");
+		inpreorderNode(root);
+	}
+	void inpreorderNode(TreeNode node)
+	{
+		if(node!=null)
+		{
+			System.out.println(node.data);
+			inpreorderNode(node.left);
+			inpreorderNode(node.right);
+		}
+	}
+	void postorder()
+	{
+		System.out.println("postorder");
+		inpostorderNode(root);
+	}
+	void inpostorderNode(TreeNode node)
+	{
+		if(node!=null)
+		{
+			inpostorderNode(node.left);
+			inpostorderNode(node.right);
+			System.out.println(node.data);
+		}
+	}
+	
 	public static void main(String[] args)
 	{
 		Binarysearchtree bst=new Binarysearchtree();
 		bst.insert(10);
+		bst.insert(13);
 		bst.insert(45);
 		bst.insert(5);
+		bst.insert(7);
+		bst.insert(4);
+		bst.insert(12);
 		bst.inorder();
+		bst.preorder();
+		bst.postorder();
 	}
 
 }
